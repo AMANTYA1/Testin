2ab958b8ebd2b8e8385861cc7434aab0fffa30ec
NullCoder <95975318+AMANTYA1@users.noreply.github.com>
2022-12-24 11:45:12 +0530

    Delete Vplay.py

--- RaiChu/Player/Vplay.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.enums import ParseMode
-
-@Client.on_message(filters.command("vplay"))
-async def banall(bot, m):
-    await bot.send_message(text=f"Fixing Soon Please wait sir 🚏 [{m.from_user.first_name}](tg://user?id={m.from_user.id})", chat_id=m.chat.id)
